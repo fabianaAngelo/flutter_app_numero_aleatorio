da3662dfe52899cc19ba4aada638027f650c65a8
Fabiana.Angelo <fabianna.info@gmail.com>
2020-09-13 09:34:48 -0300

    alterações de cores do botão e label

--- lib/widgets/numero_aleatorio.dart
+++ lib/widgets/numero_aleatorio.dart
@@ -1,36 +1,35 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 
-class NumeroAleatorio extends StatefulWidget{
+class NumeroAleatorio extends StatefulWidget {
   @override
   _NumeroAleatorioState createState() => _NumeroAleatorioState();
 }
 
-class _NumeroAleatorioState extends State<NumeroAleatorio>{
-    int _numero = 0;
+class _NumeroAleatorioState extends State<NumeroAleatorio> {
+  int _numero = 0;
 
-    void _gerarNumero(){
-      setState(() {
-        Random numeroAleatorio = new Random();
-        _numero = numeroAleatorio.nextInt(1000);
-      });
-    }
+  void _gerarNumero() {
+    setState(() {
+      Random numeroAleatorio = new Random();
+      _numero = numeroAleatorio.nextInt(1000);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-   return Container(
-     child: Column(
-     children: [
-       Text('$_numero',
-        style: TextStyle(fontSize: 28),
-        ),
-      SizedBox(height: 30),
-      RaisedButton (
-        onPressed: _gerarNumero,
-      child: Text ("Gerar número")
-     )
-    ],
-    ),
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            '$_numero',
+            style: TextStyle(fontSize: 28, backgroundColor: Colors.green),
+          ),
+          SizedBox(height: 30),
+          RaisedButton(textColor: Colors.green,
+              onPressed: _gerarNumero, child: Text("Gerar número"))
+        ],
+      ),
     );
   }
 }
